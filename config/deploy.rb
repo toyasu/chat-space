c99fb84b82e7aaeef07a1a0898df9492164bb055
toyasu <64839788+toyasu@users.noreply.github.com>
2020-05-22 20:41:51 +0900

    chatspace自動デプロイ2

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['motoyama08/.ssh/key_pem.pem'] 
+                  keys: ['~/.ssh/motoyama08.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
